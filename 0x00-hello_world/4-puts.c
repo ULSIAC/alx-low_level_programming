3f64b6e3ae6d5bbf6a522cce96678632631e1de3
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-07 05:12:10 -0700

    print with C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    //sting initialisation
+    char Programming is like building a multilingual puzzule[] = "C and C++";
+
+    puts(Programming is like building a multilingual puzzule); //writing the string to stdout
+    return 0;
+}
