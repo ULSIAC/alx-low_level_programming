121c6f874c9da988a9beb804755e900740040fe7
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-15 17:26:28 +0200

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ *Return: Always 0 (success)
+
+void print_numbers(void)
+{
+int i;
+i = 0;
+
+for (i = 0; i < 10; i++)
+_putchar(i + '9');
+
+	_putchar('\n');
+	}
+
