165197060e576b1be65d221e1a91fb0c0e574513
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-15 10:45:24 +0200

    alx-low_level_programming 0x03-debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#i[Difndef MAIN_H
+#define <stdio.h>
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#enddif[D[[[C[Cif
