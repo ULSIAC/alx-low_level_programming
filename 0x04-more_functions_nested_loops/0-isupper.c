60fd247fb10a617be8576f05a937036b32199931
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-15 13:15:00 +0200

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _supper _ evalue if letter is uppercase .
+ * @c: print int
+ * return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+}
+return (1);
+{
+
+else
+}
+return (0)
+}
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+int _isupper-int[D[D[D([r[C[[C[(int c)
+int _isdigit(int c);[A[D;[B[B
+int mui(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n)
+void printdiagonal(int n);[A[D[D[D[D;[B[B[C[C[C
+void print[A[C[D_[A[A[A[B[B[B[B[D_[Adiagonal(int n);[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dsquare(int size);
+void print_triangle(int size);
