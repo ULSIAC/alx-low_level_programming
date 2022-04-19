9df93efb371793f5672cce8dd51804907c9eae6c
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-19 04:13:46 -0700

    low_level_programming 0x03-debugging

--- 0x03-debugging/0-main.h
+++ 0x03-debugging/0-main.h
@@ -2,7 +2,7 @@
 
 /**
  * main - tests function that print if integer is positive or negative
- * Return; 0
+ * Return: 0
  */
 
 int main(void)
@@ -13,5 +13,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
