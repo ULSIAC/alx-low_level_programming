8680f14d1d32da77d7dfc2e6becda301a0854de2
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-21 15:57:41 +0200

    task 2 pointer array

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the value of two integer using a pointer
- * @a:first integer value
- * @b:second integer value
- * Return: void
+ * swap_int - swaping 2 integer using a pointer
+ * @a:integer
+ * @b:integer
+ * Return:void
  */
 void swap_int(int *a, int *b)
 {
 	int x;
-	
+
 	x = *a;
 	*a = *b;
 	*b = x;
