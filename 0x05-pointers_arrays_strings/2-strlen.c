926302b756614ab4fa2ed629f0554ed6ced78aa8
HOUNYO Ulsiac Pascal <pascalhounyo12@gmail.com>
2022-04-21 16:15:55 +0200

    random password generatorfor 101-crackme

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * strlen - lenght of string
+ * _strlen - lenght of string
  * @s:char
  * Return:int
  */
 int _strlen(char *s)
 {
 	int i;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		continue;
-	}
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
 return (i);
 }
